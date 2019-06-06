3c0d3844be2b65536e702c11ab3960b96c63043f
karelvandoesburg <karelvandoesburg@gmail.com>
2019-06-06 14:58:11 +0200

    first steps, but apparently Timo is working on it

---
+++ src/Cart.API/Controllers/UserController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using GrainInterfaces;
+using GrainInterfaces.States;
+using Microsoft.AspNetCore.Mvc;
+using Orleans;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace Cart.API.Controllers
+{
+    [Route("api/users")]
+    public class UserController : Controller
+    {
+        private readonly IClusterClient _client;
+
+        public UserController(IClusterClient client)
+        {
+            _client = client;
+        }
+
+        [HttpPost("create/{id}")]
+        public async Task<Guid> CreateUser(Guid id)
+        {
+            var grain = _client.GetGrain<IUserGrain>(id);
+
+        }
+
+        [HttpDelete("remove/{id}")]
+
+        
+        [HttpGet("find/{id}")]
+
+
+        [HttpGet("credit/{id}")]
+
+
+        [HttpPost("credit/substract/{id}/{amount}")]
+
+
+        [HttpPost("credit/add/{id}/{amount}")]
+    }
+}
