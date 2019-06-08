6aee385b1d6d4e6afb10df2ac2c71c37064c33de
Mathieu <mathieupost@gmail.com>
2019-06-08 15:43:57 +0200

    Oeps

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
-sed 's/THIS_STRING_IS_REPLACED_DURING_BUILD/'$(date)'/g' deployment.yml | kubectl apply -f -
+sed 's/THIS_STRING_IS_REPLACED_DURING_BUILD/$(date)/g' deployment.yml | kubectl apply -f -
 
