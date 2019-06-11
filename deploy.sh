274ef0371080941babc23f7b04fd5c4a8b3620ef
Mathieu <mathieupost@gmail.com>
2019-06-11 23:00:50 +0200

    Fix deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,18 @@
 #!/usr/bin/env bash
 
-sed 's/THIS_STRING_IS_REPLACED_DURING_BUILD/$(date)/g' deployment.yml | kubectl apply -f -
+kubectl get pods | grep Evicted | awk '{print $1}' | xargs kubectl delete pod
+
+API_COUNT=1
+SILO_COUNT=1
+
+while getopts s:a: option
+do
+case "${option}"
+in
+s) SILO_COUNT=${OPTARG};;
+a) API_COUNT=${OPTARG};;
+esac
+done
+
+bash -c "sed 's/THIS_STRING_IS_REPLACED_DURING_BUILD/$(date)/g' deployment.yml | sed 's/SILO_COUNT/$SILO_COUNT/g' | sed 's/API_COUNT/$API_COUNT/g' | kubectl apply -f -"
 
